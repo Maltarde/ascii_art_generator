2739fdc2f0a0a4b2a8c1889c5feca684727b5252
Maltarde <84210619+Maltarde@users.noreply.github.com>
2025-05-11 02:18:34 +0200

    Translate comments and print messages to English in ASCII converter script

--- ascii_art.py
+++ ascii_art.py
@@ -1,22 +1,22 @@
 from PIL import Image
 
-# Caractères ASCII utilisés pour créer l'image
+# ASCII characters used to create the image
 ASCII_CHARS = "@%#*+=-:. "
 
-# Image à convertir
+# Image to convert
 while True:
-    image_path = input("Entrer le chemin de l'image: ")
+    image_path = input("Enter the path to the image: ")
     try:
         image = Image.open(image_path)
     except FileNotFoundError as e:
-        print("L'image n'a pas été trouvée!")
+        print("Image not found!")
         continue
     break
 
-# Redimensionnement de l'image
+# Resize the image
 while True:
     try:
-        new_width = int(input("Largeur de l'image final en ASCII: "))
+        new_width = int(input("Final ASCII image width: "))
         if new_width < 100 or new_width > 1000:
             raise ValueError
     except ValueError:
@@ -27,18 +27,18 @@ width, height = image.size
 new_height = int(new_width * (height / width) * 0.4)
 image = image.resize((new_width, new_height))
 
-# Convertir l'image en niveau de gris
+# Convert the image to grayscale
 image = image.convert("L")
 
-# Convertion des pixels en ASCII
+# Convert pixels to ASCII
 pixels = image.getdata()
 pixels_to_ascii = [ASCII_CHARS[pixel * (len(ASCII_CHARS) - 1) // 255] for pixel in pixels]
 
-# Mise en forme de l'image
+# Format the ASCII image
 ascii_art = ["".join(pixels_to_ascii[i:(i + new_width)]) for i in range(0, len(pixels_to_ascii), new_width)]
 ascii_art = "\n".join(ascii_art)
 
-# Sauvegarde dans un ficher
+# Save to a file
 with open("art.txt", "w") as f:
     f.write(ascii_art)
-print("Ficher ASCII créer")
+print("ASCII file created")
